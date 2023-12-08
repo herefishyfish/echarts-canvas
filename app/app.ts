740457be4d0413e94eb647888c0aa58717bd8068
Dylan Llewellyn <46717769+herefishyfish@users.noreply.github.com>
2023-12-08 14:55:56 +0800

    update canvas

--- app/app.ts
+++ app/app.ts
@@ -4,8 +4,14 @@ You can use this file to perform app-level initialization, but the primary
 purpose of the file is to pass control to the app’s first module.
 */
 
-import { Application } from '@nativescript/core'
+import { Application, Utils } from '@nativescript/core'
 import '@nativescript/canvas-polyfill';
+
+declare const jp: any;
+
+if (global.isAndroid) {
+  jp.wasabeef.takt.Takt.stock(Utils.android.getApplicationContext()).seat(jp.wasabeef.takt.Seat.TOP_CENTER).color(-65536);
+}
 
 Application.run({ moduleName: 'app-root' })
 
